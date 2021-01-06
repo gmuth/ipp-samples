6738d670f24978ab0dbb48a05bcb8aac63c0f628
Gerhard Muth <gerhard.muth@gmx.de>
2021-01-07 00:03:29 +0100

    kotlin version 1.4.10

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,7 +2,7 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
     id("java")
-    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+    id("org.jetbrains.kotlin.jvm") version "1.4.10"
 }
 
 group = "de.gmuth"
