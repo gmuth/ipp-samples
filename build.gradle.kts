d75ab152c4244a79d42102dc9bb1fee595090f2e
Gerhard Muth <gerhard.muth@gmx.de>
2021-01-06 15:39:31 +0100

    gradle build

---
+++ build.gradle.kts
@@ -0,0 +1,33 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("java")
+    id("org.jetbrains.kotlin.jvm") version "1.3.72"
+}
+
+repositories {
+    mavenLocal()
+    mavenCentral()
+    maven {
+        url = uri("https://maven.pkg.github.com/gmuth/ipp-client-kotlin")
+        credentials {
+            // set gpr.user and gpr.token in ~/.gradle/gradle.properties
+            // gpr.username=myname
+            // gpr.token=mytoken
+            username = project.findProperty("gpr.user") as String?
+            password = project.findProperty("gpr.token") as String?
+        }
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions {
+        jvmTarget = "1.6"
+    }
+}
+
+dependencies {
+    implementation(platform("org.jetbrains.kotlin:kotlin-bom"))
+    implementation("org.jetbrains.kotlin:kotlin-stdlib")
+    implementation("de.gmuth.ipp:ipp-client-kotlin:2.0-SNAPSHOT")
+}
