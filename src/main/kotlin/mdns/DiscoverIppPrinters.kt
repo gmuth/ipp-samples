0fefb403e8305db596364ae1e98d7682f51e992b
Gerhard Muth <gerhard.muth@gmx.de>
2021-01-07 23:45:41 +0100

    removed extension

--- src/main/kotlin/mdns/DiscoverIppPrinters.kt
+++ src/main/kotlin/mdns/DiscoverIppPrinters.kt
@@ -1,13 +1,14 @@
 package mdns
 
 import de.gmuth.ipp.client.IppPrinter
+import java.net.URI
 import javax.jmdns.JmDNS
 
 fun main() {
     val jmDns = JmDNS.create()
     jmDns.list("_ipp._tcp.local.").forEach {
         val printerName = it.name
-        val printerUri = it.getIppUri()
+        val printerUri = with(it) { URI.create("ipp://$server:$port/${getPropertyString("rp")}") }
         val ippPrinter = IppPrinter(printerUri)
         println("* $printerName")
         println("  $printerUri")
--- src/main/kotlin/mdns/ServiceInfoExtension.kt
+++
@@ -1,6 +0,0 @@
-package mdns
-
-import java.net.URI
-import javax.jmdns.ServiceInfo
-
-fun ServiceInfo.getIppUri(scheme: String = "ipp") = URI.create("$scheme://$server:$port/${getPropertyString("rp")}")
