5967ebe3fb84ab3b0c04c970513387f0280c88ad
Gerhard Muth <gerhard.muth@gmx.de>
2021-07-21 22:14:24 +0200

    configureSlf4j()

--- src/main/kotlin/log/IppClientLoggingDemoWithSlf4j.kt
+++ src/main/kotlin/log/IppClientLoggingDemoWithSlf4j.kt
@@ -1,8 +1,11 @@
 package log
 
-import de.gmuth.log.Slf4jLogging
+import de.gmuth.log.Logging
+
+//import de.gmuth.log.Slf4jLogging
 
 fun main() {
-    Slf4jLogging.configure()
+    //Slf4jLogging.configure() // 2.1
+    Logging.configureSlf4j() // 2.2
     IppClientLoggingDemo().log()
 }
