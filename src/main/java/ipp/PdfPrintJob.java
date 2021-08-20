8483a7facfbb7f3d246f4cc551f1591017824cc1
Gerhard Muth <gerhard.muth@gmx.de>
2021-08-20 16:51:28 +0200

    demonstrate how to disable chunking

--- src/main/java/ipp/PdfPrintJob.java
+++ src/main/java/ipp/PdfPrintJob.java
@@ -4,6 +4,7 @@ import static de.gmuth.ipp.client.IppTemplateAttributes.documentFormat;
 import static de.gmuth.ipp.client.IppTemplateAttributes.jobName;
 
 import de.gmuth.ipp.client.IppColorMode;
+import de.gmuth.ipp.client.IppConfig;
 import de.gmuth.ipp.client.IppJob;
 import de.gmuth.ipp.client.IppPrinter;
 import de.gmuth.ipp.client.IppSides;
@@ -12,7 +13,12 @@ import java.io.File;
 public class PdfPrintJob {
 
   public static void main(String[] args) {
-    IppPrinter ippPrinter = new IppPrinter("ipp://localhost:8632/printers/laser");
+
+    IppConfig ippConfig = new IppConfig();
+    ippConfig.setChunkedStreamingMode(false);
+    //HttpURLConnectionClient.Companion.getLog().setLogLevel(LogLevel.DEBUG);
+
+    IppPrinter ippPrinter = new IppPrinter("ipp://localhost:8632/printers/laser", ippConfig);
     ippPrinter.logDetails();
 
     File file = new File("test-docs/A4-ten-pages.pdf");
